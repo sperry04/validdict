30ade19e052abb630db2cc3febe324f6c24e8da8
sperry <sperry04@users.noreply.github.com>
2024-07-29 12:28:50 -0700

    Added additional contextual validators to the API imports.

--- validdict/__init__.py
+++ validdict/__init__.py
@@ -2,7 +2,7 @@
 from .validator import Or, Any, Outcome
 from .key import KeyValidator, RequiredKey, OptionalKey, OtherKeys, StartsWith
 from .scalars import Str, Num, Bool, Regex
-from .contextual import CallbackValidator
+from .contextual import CallbackValidator, CallbackKeyValidator, ContextualValidator
 from .seq import Seq
 from .map import Map
 from .schema import Schema
